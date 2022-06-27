a2f825edc0c587516f42f0c6bfa4672a5d2cbd92
Michael <67665934+mjstafford@users.noreply.github.com>
2022-06-27 15:11:52 -0700

    updated with JS

--- profile-page/script.js
+++ profile-page/script.js
@@ -1,1 +1,35 @@
 console.log("page loaded...");
+
+function changeName() {
+  let newName = prompt("Please enter new profile name:")
+  let profileNameTag = document.querySelector(".card-body h1")
+  if (newName === null) {
+    alert("canceled")
+  } else if (!newName || newName.trim().length === 0) {
+    alert("need to add a real name!")
+  } else {
+    profileNameTag.innerHTML = newName
+  }
+}
+
+function approveConnection(element) {
+  //decrease requests connections #
+  let numConnectionsRequestsElement = document.getElementsByClassName("badge")[0]
+  let numConnectionsRequests = parseInt(numConnectionsRequestsElement.innerHTML)
+  numConnectionsRequestsElement.innerHTML = numConnectionsRequests - 1
+
+  //increase connections
+  let numConnectionsElement = document.getElementsByClassName("badge")[1]
+  let numConnections = parseInt(numConnectionsElement.innerHTML)
+  numConnectionsElement.innerHTML = numConnections + 1
+
+  //remove element
+  element.parentElement.parentElement.remove()
+}
+
+function removeConnection(element) {
+  let numConnectionsElement = document.getElementsByClassName("badge")[0]
+  let numConnections = parseInt(numConnectionsElement.innerHTML)
+  numConnectionsElement.innerHTML = numConnections - 1
+  element.parentElement.parentElement.remove()
+}
