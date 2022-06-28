be10b97b74d7f265f018d3efcff836b8a331b5a9
Michael <67665934+mjstafford@users.noreply.github.com>
2022-06-28 15:48:29 -0700

    refactored the temperature function & assoc. html

--- AdvancedTopics/ArrayChallenges/script.js
+++ AdvancedTopics/ArrayChallenges/script.js
@@ -21,8 +21,31 @@ alwaysHungry([4, 1, 5, 7, 2]);
 function highPass(arr, cutoff) {
   var filteredArr = [];
   // your code here
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > cutoff) {
+      filteredArr.push(arr[i])
+    }
+  }
   return filteredArr;
 }
 var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
 console.log(result); // we expect back [6, 8, 10, 9]
 
+
+// 3. Better than average
+function betterThanAverage(arr) {
+  var sum = 0;
+  // calculate the average
+  var count = 0
+  // count how many values are greated than the average
+
+  sum = arr.reduce((pre, cur) => {
+    return pre + cur
+  })
+
+  return sum
+
+  return count;
+}
+var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
+console.log(result); // we expect back 4
--- dojoWeather/script.js
+++ dojoWeather/script.js
@@ -3,31 +3,47 @@ let hideBox = () => {
   document.querySelector("#cookie").remove()    //completely removes the cookie div
 }
 
-function updateTemp(element) {
-  // debugger
-  let allHighElements = document.querySelectorAll(".high-temp")
-  let allLowElements = document.querySelectorAll(".low-temp")
-
-  if (element.value === "F") {
-    allHighElements.forEach(element => {
-      let oldHotTemp = element.innerText.split(" ")[0]
-      element.innerHTML = `${Math.round((oldHotTemp * 9 / 5) + 32)} <span class="hot-degree">&deg;</span>`
-    })
-
-    allLowElements.forEach(element => {
-      let oldColdTemp = element.innerText.split(" ")[0]
-      element.innerHTML = `${Math.round((oldColdTemp * 9 / 5) + 32)} <span class="cold-degree">&deg;</span>`
-    })
-  } else {
-    //convert back to C
-    allHighElements.forEach(element => {
-      let oldHotTemp = element.innerText.split(" ")[0]
-      element.innerHTML = `${Math.round((oldHotTemp - 32) * (5 / 9))} <span class="hot-degree">&deg;</span>`
-    })
-
-    allLowElements.forEach(element => {
-      let oldColdTemp = element.innerText.split(" ")[0]
-      element.innerHTML = `${Math.round((oldColdTemp - 32) * (5 / 9))} <span class="cold-degree">&deg;</span>`
-    })
-  }
+let functionFactory = {
+  "F": (temp) => Math.round((temp * (9 / 5) + 32)),
+  "C": (temp) => Math.round((temp - 32) * 5 / 9)
 }
+
+function refactoredUpdateTemp(element) {
+  let allTempElements = document.querySelectorAll(".low-temp, .high-temp")
+  allTempElements.forEach(item => {
+    if (element.value === "F") {
+      item.innerHTML = functionFactory["F"](item.innerHTML)
+    } else {
+      item.innerHTML = functionFactory["C"](item.innerHTML).toPrecision(2)
+    }
+  })
+}
+
+// function updateTemp(element) {
+//   debugger
+//   let allHighElements = document.querySelectorAll(".high-temp")
+//   let allLowElements = document.querySelectorAll(".low-temp")
+
+//   if (element.value === "F") {
+//     allHighElements.forEach(element => {
+//       let oldHotTemp = element.innerText.split(" ")[0]
+//       element.innerHTML = `${Math.round((oldHotTemp * 9 / 5) + 32)} <span class="hot-degree">&deg;</span>`
+//     })
+
+//     allLowElements.forEach(element => {
+//       let oldColdTemp = element.innerText.split(" ")[0]
+//       element.innerHTML = `${Math.round((oldColdTemp * 9 / 5) + 32)} <span class="cold-degree">&deg;</span>`
+//     })
+//   } else {
+//     //convert back to C
+//     allHighElements.forEach(element => {
+//       let oldHotTemp = element.innerText.split(" ")[0]
+//       element.innerHTML = `${Math.round((oldHotTemp - 32) * (5 / 9))} <span class="hot-degree">&deg;</span>`
+//     })
+
+//     allLowElements.forEach(element => {
+//       let oldColdTemp = element.innerText.split(" ")[0]
+//       element.innerHTML = `${Math.round((oldColdTemp - 32) * (5 / 9))} <span class="cold-degree">&deg;</span>`
+//     })
+//   }
+// }
