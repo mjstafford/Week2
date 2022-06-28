f30aebf287cc0ae5eab8d6d503be024e5f14bdb2
Michael <67665934+mjstafford@users.noreply.github.com>
2022-06-28 14:19:28 -0700

    arrayChallenges in progress

---
+++ AdvancedTopics/ArrayChallenges/script.js
@@ -0,0 +1,28 @@
+// 1.Always Hungry
+function alwaysHungry(arr) {
+  let hasFood = false
+  arr.forEach(element => {
+    if (element === "food") {
+      hasFood = true
+      console.log("yummy")
+    }
+  });
+  if (!hasFood) {
+    console.log("I'm hungry")
+  }
+}
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
+
+
+// 2. High Past Filter
+function highPass(arr, cutoff) {
+  var filteredArr = [];
+  // your code here
+  return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
+
