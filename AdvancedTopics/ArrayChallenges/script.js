4a819692536c32ab36e826015b932ae0daf3fd2d
Michael <67665934+mjstafford@users.noreply.github.com>
2022-06-28 18:45:39 -0700

    completed

--- AdvancedTopics/ArrayChallenges/script.js
+++ AdvancedTopics/ArrayChallenges/script.js
@@ -35,17 +35,33 @@ console.log(result); // we expect back [6, 8, 10, 9]
 // 3. Better than average
 function betterThanAverage(arr) {
   var sum = 0;
-  // calculate the average
-  var count = 0
-  // count how many values are greated than the average
+  var count = 0;
 
   sum = arr.reduce((pre, cur) => {
     return pre + cur
   })
 
-  return sum
+  var avg = sum / arr.length
 
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > avg) { count++ }
+  }
   return count;
 }
 var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
 console.log(result); // we expect back 4
+
+
+//5. Fibonacci Array
+function fibonacciArray(n) {
+  // the [0, 1] are the starting values of the array to calculate the rest from
+  var fibArr = [0, 1];
+  // your code here
+  for (var i = 2; i < n; i++) {
+    fibArr.push(fibArr[i - 2] + fibArr[i - 1])
+  }
+  return fibArr;
+}
+
+var result = fibonacciArray(10);
+console.log(result); // we expect back [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
