e37195cbffe8e0223be955e4c53d4dc99cf259f0
Michael <67665934+mjstafford@users.noreply.github.com>
2022-06-30 13:48:01 -0700

    a working clock

---
+++ AdvancedTopics/workingClock/script.js
@@ -0,0 +1,23 @@
+let secondsHand = document.querySelector("#seconds")
+let minutesHand = document.querySelector("#minutes")
+let hourHand = document.querySelector("#hour")
+
+function getSecondsSinceStartOfDay() {
+  return new Date().getSeconds() +
+    new Date().getMinutes() * 60 +
+    new Date().getHours() * 3600;
+}
+
+setInterval(function () {
+  var time = getSecondsSinceStartOfDay();
+  console.log(time);
+  // your code here
+  let secondsDegree = 180 + (time % 60) * 6         //add 180 to get the rotation to start based off of 12
+  let minutesDegree = 180 + ((time / 60) % 60) * 6  // time/60 represents minutes, %60 represents current minute, 6 represents 1 minute movement in degrees
+  let hourDegree = 180 + ((time / 3600) % 12) * 30   // time/3600 gives total hours, %12 gives current hour, 30 is how much the hour hand moves in a given hour
+  secondsHand.style.transform = `rotate(${secondsDegree}deg)`
+  minutesHand.style.transform = `rotate(${minutesDegree}deg)`
+  hourHand.style.transform = `rotate(${hourDegree}deg)`
+
+  secondsDegree += 6
+}, 1000);
