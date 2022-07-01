3e8cb852dff689eda44d19eadc2662af2e1ea413
Michael <67665934+mjstafford@users.noreply.github.com>
2022-07-01 09:57:05 -0700

    dojo sweeper - no bonus challenge

---
+++ algo1/game.js
@@ -0,0 +1,71 @@
+var theDojo = [
+  [1, 0, 1, 1, 1, 0, 4, 0, 8, 0],
+  [3, 1, 0, 7, 0, 0, 6, 0, 8, 8],
+  [5, 0, 7, 0, 3, 6, 6, 6, 0, 0],
+  [2, 3, 0, 9, 0, 0, 6, 0, 8, 0],
+  [6, 0, 3, 3, 0, 2, 0, 3, 0, 4],
+  [0, 0, 3, 3, 0, 0, 2, 2, 3, 0],
+  [0, 0, 0, 0, 5, 0, 1, 2, 0, 6],
+  [2, 2, 2, 2, 0, 7, 1, 1, 1, 0],
+  [5, 2, 0, 2, 0, 0, 0, 1, 1, 2],
+  [9, 2, 2, 2, 0, 7, 0, 1, 1, 0]];
+var dojoDiv = document.querySelector("#the-dojo");
+
+// Creates the rows of buttons for this game
+function render(theDojo) {
+  var result = "";
+  for (var i = 0; i < theDojo.length; i++) {
+    for (var j = 0; j < theDojo[i].length; j++) {
+      result += `<button class="tatami" onclick="howMany(${i}, ${j}, this)"></button>`;
+    }
+  }
+  return result;
+}
+
+// TODO - Make this function tell us how many ninjas are hiding 
+//        under the adjacent (all sides and corners) squares.
+//        Use i and j as the indexes to check theDojo.
+function howMany(i, j, element) {
+  console.log({ i, j });
+  var sum = 0
+
+  //find index of all neighbors (where i represents row index and j represents column)
+  var neighbors = [
+    [i - 1, j - 1], [i - 1, j], [i - 1, j + 1],   //top 3 
+    [i, j - 1], [i, j + 1],                       //left and right of selected
+    [i + 1, j - 1], [i + 1, j], [i + 1, j + 1]    //bottom 3
+  ]
+
+  //if neighbour are inboud add to sum
+  for (const neighbor of neighbors) {
+    let row = neighbor[0]
+    let column = neighbor[1]
+    if (verify(row, column)) {
+      sum += theDojo[row][column]
+    }
+  }
+  console.log(sum)
+  alert(`There are ${sum} ninjas near you!`)
+}
+
+//helper function verify neighbors are in bound
+function verify(row, column) {
+  return row >= 0 && row <= 9 && column >= 0 && column <= 9
+}
+
+// BONUS CHALLENGES
+// 1. draw the number onto the button instead of alerting it
+// 2. at the start randomly place 10 ninjas into theDojo with at most 1 on each spot
+// 3. if you click on a ninja you must restart the game 
+//    dojoDiv.innerHTML = `<button onclick="location.reload()">restart</button>`;
+
+// start the game
+// message to greet a user of the game
+var style = "color:cyan;font-size:1.5rem;font-weight:bold;";
+console.log("%c" + "IF YOU ARE A DOJO STUDENT...", style);
+console.log("%c" + "GOOD LUCK THIS IS A CHALLENGE!", style);
+// shows the dojo for debugging purposes
+console.table(theDojo);
+// adds the rows of buttons into <div id="the-dojo"></div> 
+dojoDiv.innerHTML = render(theDojo);
+
